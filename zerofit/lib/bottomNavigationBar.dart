0c1198e3f0021864050318c2120d328b36e04033
wagnerfilho1995 <wagnerfilho1995@gmail.com>
2020-02-14 11:03:46 -0300

    Login page, inicio firebase

---
+++ zerofit/lib/bottomNavigationBar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'treinos.dart';
+import 'body.dart';
+import 'aerobic.dart';
+
+class MyBottomNavigationBar extends StatefulWidget {
+  @override
+  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Treinos(),
+    Aerobico(),
+    Corpo()
+  ];
+
+  void onTappedBar(int index){
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTappedBar,
+        backgroundColor: Colors.green,
+        items:[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center, color: Colors.white,),
+            title: Text('Treinos', style: TextStyle(color: Colors.white),)
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run, color: Colors.white,),
+            title: Text('Aerobicos', style: TextStyle(color: Colors.white),)
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.white,),
+            title: Text('Corpo', style: TextStyle(color: Colors.white),)
+          )
+        ] 
+      ),
+    );
+  }
+}
--- zerofit/lib/login.dart
+++ zerofit/lib/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'bottomNavigationBar.dart';
 
 class Login extends StatefulWidget {
  
@@ -8,25 +9,133 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
 
+  bool is_visible = false;
+  bool isSwitched = false;
+
+  Icon set_icon_visible(){
+    if(is_visible){
+      return Icon(Icons.visibility, color: Colors.red,);
+    }
+    return Icon(Icons.visibility_off, color: Colors.red,);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(
-      //  title: const Text("Vistoria SMTT"),
-      //),
+      backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.all(10),
         child: Center(
           child: ListView(
             children: <Widget>[
               SizedBox(height: 30,),
-              Image.asset('assets/gym-logo.jpg', width: 150.0, height: 160.0),
+              Image.asset('assets/logo-z.png', width: 100.0, height: 100.0),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                child: Text('Zerofit', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold), textAlign: TextAlign.center,)
+                ),
               SizedBox(height: 30,),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: TextField()
+                child: TextField(
+                  style: new TextStyle(fontSize: 16, color: Colors.red),
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    labelText: "Email ou usuário",
+                    labelStyle: TextStyle(color: Colors.red),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.red)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(width: 2, color: Colors.red)),
+                      suffixIcon: FlatButton(
+                      child: Icon(Icons.person, color: Colors.red,),
+                      onPressed: (){
+                      },
+                    )
+                  )
+                )
               ),
-
+              SizedBox(height: 10,),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  style: new TextStyle(fontSize: 16, color: Colors.blue),
+                  autofocus: true,
+                  obscureText: is_visible == true? false : true,
+                  decoration: InputDecoration(
+                    labelText: "Digite sua senha",
+                    labelStyle: TextStyle(color: Colors.red),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.red)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(width: 2, color: Colors.red)),
+                    suffixIcon: FlatButton(
+                        onPressed: (){
+                          setState(() {
+                            is_visible = !is_visible;
+                          });
+                      }, 
+                      child: set_icon_visible(),
+                    )
+                  )
+                )
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  Text('Manter conectado', style: TextStyle(color: Colors.red),),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[
+                      Switch(
+                      value: isSwitched,
+                      onChanged: (value){
+                        setState(() {
+                          isSwitched = value;
+                        });
+                      },
+                      activeTrackColor: Colors.red[300],
+                      activeColor: Colors.red,
+                    )
+                    ]
+                  ),
+                ]
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: ButtonTheme(
+                  height: 60,
+                  child: RaisedButton(
+                    color: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.red)
+                    ),
+                    child: Text('Entrar', style: TextStyle(color: Colors.white),),
+                    onPressed: (){
+                      Navigator.push(
+                        context, 
+                        MaterialPageRoute(builder : (context) => MyBottomNavigationBar()),
+                      );
+                    }
+                  )
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text('Criar conta', style: TextStyle(color: Colors.red), textAlign: TextAlign.center,)
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Text('Esqueceu a senha?', style: TextStyle(color: Colors.red), textAlign: TextAlign.center,),
+                )
+              )
           ],
         ),
         ),
@@ -34,3 +143,4 @@ class _LoginState extends State<Login> {
     );
   }
 }
+
